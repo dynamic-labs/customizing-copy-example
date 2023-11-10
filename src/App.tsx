3113d2ff17e891a9e90b3e406496cf77ff3f6e16
Matthew Foyle <mfoyle93@gmail.com>
2023-11-10 13:21:04 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./App.css";
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+  LocaleResource,
+} from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+
+function App() {
+  const signMessageCopy: LocaleResource = {
+    en: {
+      dyn_sign_message: {
+        sign_button: "Confirm",
+        title: "Confirm it's you to continue",
+      },
+    },
+  };
+
+  return (
+    <div className="App">
+      <DynamicContextProvider
+        settings={{
+          environmentId: "f0b977d0-b712-49f1-af89-2a24c47674da",
+          walletConnectors: [EthereumWalletConnectors],
+        }}
+        locale={signMessageCopy}
+      >
+        <DynamicWidget />
+      </DynamicContextProvider>
+    </div>
+  );
+}
+
+export default App;
